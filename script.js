cbffbf80e190bf46e69a52a67ce830b979514167
pallavranpise <runpsaypallove@gmail.com>
2022-08-19 22:54:53 +0530

    added buttons to change grid size

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-let n = 2;
+let n = 1;
 const container = document.querySelector(".container");
-const mybutton = document.createElement("button");
-mybutton.innerHTML = "play";
-document.querySelector('body').insertBefore(mybutton,container);
+const play = document.createElement("button");
+const decrease = document.createElement("button");
+const increase = document.createElement("button");
+
+play.innerHTML = "play"
+increase.innerHTML = "increase";
+decrease.innerHTML = "decrease";
+document.querySelector('body').insertBefore(play,container);
+document.querySelector('body').insertBefore(increase,container);
+document.querySelector('body').insertBefore(decrease,container);
+
+increase.addEventListener("click",()=>{n++;makeGrid()});
+decrease.addEventListener("click",()=>{n--;makeGrid()});
 
 
-mybutton.addEventListener("click", () => {
+function makeGrid(){
     container.innerHTML = '';
-    n++;
+    const widthe = parseInt(window.getComputedStyle(container).width, 10);
+    
     for (let index = 1; index <= n*n; index++) {
-        const mydiv = document.createElement("button");
-        // mydiv.classList.add('flipped');
-        mydiv.style.cssText=`width:${Math.floor(500/(n))}px;height:${Math.floor(500/(n))}px`;
-        mydiv.innerHTML = index;
 
+        const mydiv = document.createElement("button");
+        mydiv.classList.add('flipped');
+
+        mydiv.style.cssText=`width:${Math.floor(widthe/(n))}px;height:${Math.floor(widthe/(n))}px`;
+        mydiv.innerHTML = index;
+        
         container.appendChild(mydiv);
     }
+    flipped();
+}
+
+function flipped(){
+    
+    const cards = container.querySelectorAll('.flipped');
+    cards.forEach(key=>key.addEventListener('mouseover',function() {
+        key.style.cssText += 'filter: brightness(50%);';
+    }));
+    
+}
+
+
+
+play.addEventListener("click", () => {
+    makeGrid();
+    
 });
 
-const cards = document.querySelector('button');
-Array.from(cards).forEach(card => {
-    console.log('card');
-  card.addEventListener( 'click', function() {
-    // card.classList.add('is-flipped');
-    card.innerHTML = 'b';
-  });
-})
+
