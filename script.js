d7085664b9aeb61cb8e35696e51438c0bb58da3b
pallavranpise <runpsaypallove@gmail.com>
2022-08-20 01:08:00 +0530

    solved small alignment issues

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let n = 9;
+const controls = document.createElement("div");
 const container = document.querySelector(".container");
 const play = document.createElement("button");
 const decrease = document.createElement("button");
@@ -10,15 +11,26 @@ play.innerHTML = "play"
 increase.innerHTML = "increase";
 decrease.innerHTML = "decrease";
 mode.innerHTML = "mode";
-document.querySelector('body').insertBefore(play,container);
-document.querySelector('body').insertBefore(increase,container);
-document.querySelector('body').insertBefore(decrease,container);
-document.querySelector('body').insertBefore(mode,container);
+document.querySelector('body').insertBefore(controls,container);
+controls.appendChild(play);
+controls.appendChild(increase);
+controls.appendChild(decrease);
+controls.appendChild(mode);
 
 
+
+play.addEventListener("click", () => {makeGrid();});
 increase.addEventListener("click",()=>{n++;makeGrid()});
 decrease.addEventListener("click",()=>{n--;makeGrid()});
-mode.addEventListener("click",()=>{grayMode++;grayMode=(grayMode%3);makeGrid()});
+mode.addEventListener("click",()=>{
+    if(grayMode===0)mode.innerHTML='black';
+    if(grayMode===1)mode.innerHTML='colors';
+    if(grayMode===2)mode.innerHTML='gray';
+
+    grayMode++;
+    grayMode=(grayMode%3);
+    makeGrid()
+});
 
 
 makeGrid();
@@ -27,12 +39,10 @@ function makeGrid(){
     container.innerHTML = '';
     const containerWidth = parseInt(window.getComputedStyle(container).width, 10);
     for (let index = 1; index <= n*n; index++) {
-
         const mydiv = document.createElement("div");
         mydiv.classList.add('flipped');
         mydiv.style.cssText=`width:${Math.floor(containerWidth/(n))}px;height:${Math.floor(containerWidth/(n))}px`;
-        container.appendChild(mydiv);
-        
+        container.appendChild(mydiv);       
     }
     flipped(grayMode);
 }
@@ -42,13 +52,10 @@ function flipped(grayMode){
     cards.forEach(key=>key.addEventListener('mouseover',function() {
         if(grayMode===0){
         const itemColor = Number(this.style.backgroundColor.slice(-4,-1));
-        console.log(itemColor);
         this.style.backgroundColor = `rgba(0,0,0,${itemColor + 0.1})`;
         }else if(grayMode===1){
-        this.style.backgroundColor = '#111';
+        this.style.backgroundColor = '#333';
         }else{
-        // const itemColor = Number(this.style.backgroundColor.slice(-4,-1));
-        // console.log(itemColor);
         this.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
         }
         
@@ -58,9 +65,5 @@ function flipped(grayMode){
 
 
 
-play.addEventListener("click", () => {
-    makeGrid();
-    
-});
 
 
